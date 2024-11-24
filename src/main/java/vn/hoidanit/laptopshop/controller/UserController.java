5a86e5dcdf540850af2aaa1ba19734ac71827f70
Ngpthao <phthaonguyen9999@gmail.com>
2024-11-24 12:30:24 +0100

    Initial Repository part

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,20 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
+    // private final UserRepository userRepository;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomepage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("thao", test);
+        model.addAttribute("thao", "test");
         model.addAttribute("test", "from controller with model");
         return "hello";
     }
@@ -34,6 +36,7 @@ public class UserController {
     @RequestMapping(value = "admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User nUser) {
         System.out.println("run this command" + nUser);
+        this.userService.handleSaveUser(nUser);
         return "hello";
     }
 }
